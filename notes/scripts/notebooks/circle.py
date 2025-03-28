daa41f684814b26eaa684569b30373ff58e67ef2
Nithin <nithin.v82@outlook.com>
2025-03-28 07:56:45 +0530

    started object oriented programming. Added Introduction

---
+++ notes/scripts/notebooks/circle.py
@@ -0,0 +1,15 @@
+pi = 3.14159
+def area(r):
+    return pi*r**2
+
+def circumfrance(r):
+    return 2*pi*r 
+
+def sphere_surface(r):
+    return 4.0*area(r)
+
+def sphere_volume(r):
+    return (4.0/3.0)*pi*(r**3)
+
+
+    
